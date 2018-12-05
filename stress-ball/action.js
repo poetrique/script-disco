e6d99cedbe039a1452bc8164de819b13368d2407
nullthefirst <jrandangana@gmail.com>
2018-12-05 07:01:21 +0100

    progress

--- stress-ball/action.js
+++ stress-ball/action.js
@@ -1,10 +1,4 @@
-var body = document.querySelector("body");
-var x = document.getElementById("hit").onclick;
-
-/* for(x=)
-
-alert( hit + "hits"); */
-
-for(x = 0; x < body.length; x++) {
-  console.log(x + "number of Squeezes :)");
+function hit() {
+  var x = document.getElementById("hit").onclick;
+  alert(x + x + "number of clicks :)");
 }
