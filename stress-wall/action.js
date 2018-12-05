1f9ee562dc5135e3625bdcf62d0bff7ae96443fd
nullthefirst <jrandangana@gmail.com>
2018-12-05 07:59:22 +0100

    trimmed css

---
+++ stress-wall/action.js
@@ -0,0 +1,16 @@
+function red() {
+  document.getElementById("red").style.backgroundColor = "#3cb371";
+}
+function blue() {
+  document.getElementById("blue").style.backgroundColor = "#fff";
+  document.getElementById("blue").style.borderColor = "#3cb371";
+  document.getElementById("blue").style.borderSize = "1em";
+}
+function yellow() {
+  document.getElementById("yellow").style.backgroundColor = "#3cb371";
+}
+function black() {
+  document.getElementById("red").style.backgroundColor = "#000";
+  document.getElementById("blue").style.backgroundColor = "#000";
+  document.getElementById("yellow").style.backgroundColor = "#000";
+}
