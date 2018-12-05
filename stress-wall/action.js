cdd9a1ba3ae17ecba4bd7e468c0d8ad9e0158969
nullthefirst <jrandangana@gmail.com>
2018-12-05 09:05:06 +0100

    art piece ready

--- stress-wall/action.js
+++ stress-wall/action.js
@@ -1,5 +1,5 @@
 function one() {
-  document.getElementsById("one").style.backgroundColor = "#fff";
+  document.getElementById("one").style.backgroundColor = "#fff";
 }
 function two() {
   document.getElementById("two").style.backgroundColor = "#000";
@@ -7,8 +7,32 @@ function two() {
 function three() {
   document.getElementById("three").style.backgroundColor = "#fff";
 }
-function black() {
+function four() {
+  document.getElementById("four").style.backgroundColor = "#000";
+}
+function five() {
+  document.getElementById("five").style.backgroundColor = "#fff";
+}
+function six() {
+  document.getElementById("six").style.backgroundColor = "#000";
+}
+function seven() {
+  document.getElementById("seven").style.backgroundColor = "#fff";
+}
+function eight() {
+  document.getElementById("eight").style.backgroundColor = "#000";
+}
+function nine() {
+  document.getElementById("nine").style.backgroundColor = "#fff";
+}
+function art() {
   document.getElementById("one").style.backgroundColor = "#eb9605";
   document.getElementById("two").style.backgroundColor = "#f0f0f0";
   document.getElementById("three").style.backgroundColor = "#eb9605";
+  document.getElementById("four").style.backgroundColor = "#f0f0f0";
+  document.getElementById("five").style.backgroundColor = "#eb9605";
+  document.getElementById("six").style.backgroundColor = "#f0f0f0";
+  document.getElementById("seven").style.backgroundColor = "#eb9605";
+  document.getElementById("eight").style.backgroundColor = "#f0f0f0";
+  document.getElementById("nine").style.backgroundColor = "#eb9605";
 }
