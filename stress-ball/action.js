cdfc3260d7eab35c8d8aa59d507ccb7ed75f617a
nullthefirst <jrandangana@gmail.com>
2018-12-05 06:52:59 +0100

    pseudocode js

---
+++ stress-ball/action.js
@@ -0,0 +1,10 @@
+var body = document.querySelector("body");
+var x = document.getElementById("hit").onclick;
+
+/* for(x=)
+
+alert( hit + "hits"); */
+
+for(x = 0; x < body.length; x++) {
+  console.log(x + "number of Squeezes :)");
+}
